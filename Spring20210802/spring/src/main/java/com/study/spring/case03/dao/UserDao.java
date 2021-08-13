7efa1326b0f6cd7378faf7ab112c8cfffee9a607
johnnywu0000 <88327774+johnnywu0000@users.noreply.github.com>
2021-08-13 22:24:46 +0800

    Delete Spring20210802 directory

--- Spring20210802/spring/src/main/java/com/study/spring/case03/dao/UserDao.java
+++
@@ -1,5 +0,0 @@
-package com.study.spring.case03.dao;
-
-public interface UserDao {
-	public void createUser();
-}
--- Spring20210802/spring/src/main/java/com/study/spring/case03/service/UserService.java
+++
@@ -1,5 +0,0 @@
-package com.study.spring.case03.service;
-
-public interface UserService {
-	public void addUser();
-}
